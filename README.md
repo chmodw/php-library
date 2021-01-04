4b8f9917ca28cc1bf71aacad55ece0645227bd8b
chamodya wimansha <chamodyawimansha@gmail.com>
2021-01-04 19:57:15 +0530

    database sql file create for references

---
+++ README.md
@@ -0,0 +1,9 @@
+1. The librarian, students and the professors can register and login to the system
+2. Any of the above users can update their profile details including the password
+3. The authorized users can reserve a book for 24hours
+4. The authorized users can check book availability
+5. The librarian can check the member status and update the lending details (If the book is for the lending)
+6. The system allows the Librarian to create the books catalog, add/delete books and maintain the books catalog.
+7. The librarian can update the book return details
+8. The librarian can calculate the fine for the late returns if necessary
+9. The users can search the books according to the categories (Novels, Frictions, etc.)
---
+++ library.sql
@@ -0,0 +1,236 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 04, 2021 at 03:25 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `library`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors`
+--
+
+CREATE TABLE `authors` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `createdOn` timestamp NULL DEFAULT current_timestamp(),
+  `updatedOn` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookauthors`
+--
+
+CREATE TABLE `bookauthors` (
+  `id` int(11) NOT NULL,
+  `bookId` int(11) NOT NULL,
+  `authorId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `categoryId` int(11) DEFAULT NULL,
+  `ISBN` int(11) DEFAULT NULL,
+  `createdOn` timestamp NULL DEFAULT current_timestamp(),
+  `updatedOn` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrowings`
+--
+
+CREATE TABLE `borrowings` (
+  `id` int(11) NOT NULL,
+  `bookId` int(11) DEFAULT NULL,
+  `issuedTo` int(11) NOT NULL COMMENT 'user id',
+  `issuedBy` int(11) NOT NULL COMMENT 'issued user id',
+  `issuedOn` timestamp NOT NULL DEFAULT current_timestamp(),
+  `returnDate` timestamp NULL DEFAULT NULL,
+  `retrunStatus` tinyint(1) NOT NULL DEFAULT 0,
+  `extraDayFine` int(11) NOT NULL COMMENT 'fine for a day',
+  `fine` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `createdOn` timestamp NULL DEFAULT current_timestamp(),
+  `updatedOn` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `email` varchar(245) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `authorized` tinyint(1) NOT NULL DEFAULT 0,
+  `role` enum('librarian','student','professor','') NOT NULL,
+  `dateCreated` date NOT NULL DEFAULT current_timestamp(),
+  `dateUpdated` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bookauthors`
+--
+ALTER TABLE `bookauthors`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bookId` (`bookId`),
+  ADD KEY `authorId` (`authorId`);
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoryId` (`categoryId`);
+
+--
+-- Indexes for table `borrowings`
+--
+ALTER TABLE `borrowings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bookId` (`bookId`),
+  ADD KEY `issuedTo` (`issuedTo`),
+  ADD KEY `issuedBy` (`issuedBy`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `bookauthors`
+--
+ALTER TABLE `bookauthors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `borrowings`
+--
+ALTER TABLE `borrowings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `authors`
+--
+ALTER TABLE `authors`
+  ADD CONSTRAINT `authors_ibfk_1` FOREIGN KEY (`id`) REFERENCES `bookauthors` (`authorId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `bookauthors`
+--
+ALTER TABLE `bookauthors`
+  ADD CONSTRAINT `bookauthors_ibfk_1` FOREIGN KEY (`bookId`) REFERENCES `books` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `books`
+--
+ALTER TABLE `books`
+  ADD CONSTRAINT `books_ibfk_3` FOREIGN KEY (`id`) REFERENCES `borrowings` (`bookId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `category`
+--
+ALTER TABLE `category`
+  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`id`) REFERENCES `books` (`categoryId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id`) REFERENCES `borrowings` (`issuedTo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
