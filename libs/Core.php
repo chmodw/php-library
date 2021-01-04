d7af8c596c9c11801159eb22eb877c8668fd1c84
chamodya wimansha <chamodyawimansha@gmail.com>
2021-01-04 13:19:38 +0530

    core class created

---
+++ libs/Core.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Core class.
+ *
+ * PHP Version 8.0.0
+ *
+ * @category PHP-Library
+ * @package  PHP-Library
+ * @author   Chamodya Wimansha <chamodyawimansha@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://example.com/PHP-Library
+ */
+
+class Core
+{
+    /**
+     * Function Run.
+     *
+     * @param null
+     *
+     * @return null
+     */
+    public static function run()
+    {
+        echo "run()";
+    }
+}
