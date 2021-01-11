37f8cec0c967cd6adf9ba6f57654743584ac728a
chamodya wimansha <chamodyawimansha@gmail.com>
2021-01-11 09:31:31 +0530

    required fileds added to the login form inputes

--- app/views/login.view.php
+++ app/views/login.view.php
@@ -5,15 +5,15 @@
         <div id="login-form-header">
             <h1>Library Management System</h1>
         </div>
-        <form id="login-form">
+        <form id="login-form" method="POST" action="<?php echo SITE_HOME; ?>users/login">
             <div class="row">
                 <div class="five columns">
                     <label for="Username">Username</label>
-                    <input class="u-full-width" type="text" placeholder="Username" id="Username">
+                    <input class="u-full-width" type="text" placeholder="Username" id="Username" required>
                 </div>
                 <div class="five columns">
                     <label for="Username">Password</label>
-                    <input class="u-full-width" type="password" placeholder="Password" id="Password">
+                    <input class="u-full-width" type="password" placeholder="Password" id="Password" required>
                 </div>
                 <div class="two columns">
                     <label for="submit">&nbsp;</label>
