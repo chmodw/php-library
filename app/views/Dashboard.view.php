fcb914452ea6f401c3f045ba7c7bd56c0a069be8
chamodya wimansha <chamodyawimansha@gmail.com>
2021-01-11 09:56:43 +0530

    jquery added

--- app/views/Dashboard.view.php
+++ app/views/Dashboard.view.php
@@ -2,6 +2,53 @@
 
 <?php include VIEW_PATH . "inc/nav.php"; ?>
 
+<nav class="nav-show">
+    <div class="container">
+        <ul>
+            <li><a href="#home">Goblin Hammer</a></li>
+            <li><a href="#spoilers">Spoilers</a></li>
+            <li>
+                <a href="#decks">Decks</a>
+                <ul>
+                    <li><a href="#">Decks to Beat</a></li>
+                    <li><a href="#">Community Decks</a></li>
+                </ul>
+            </li>
+            <li><a href="#combos">Combos</a></li>
+            <li>
+                <a href="#manage">Manage</a>
+                <ul>
+                    <li><a href="#">My Decks</a></li>
+                    <li><a href="#">My Combos</a></li>
+                    <li><a href="#">My Collection</a></li>
+                    <li><a href="#">My Shop</a></li>
+                </ul>
+            </li>
+            <li class="u-pull-right"><a href="#login">Login</a></li>
+            <li class="right"><a href="#register">Register</a></li>
+            <li class="icon">
+                <a href="javascript:void(0);" onclick="toggleMobileNav();">&#9776;</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 
+<div class="container">
+    <div class="row">
+        <div class="one-half column">
+            <h4>Basic Page</h4>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis, lacus vel accumsan finibus, dolor nibh auctor libero, quis hendrerit augue sapien eget dui. Nam iaculis euismod orci sit amet facilisis. Nullam ligula eros, ultricies eu feugiat nec, varius ut elit. Maecenas ac placerat tortor. Vestibulum quis dui in nisi congue faucibus nec fermentum velit. Aliquam tempus finibus justo venenatis porttitor. Fusce non fringilla est, in accumsan lectus. Maecenas quis odio id sem ultricies posuere mattis ac lectus. Sed sit amet feugiat elit, at euismod orci.</p>
+
+            <p>Cras aliquam consectetur tellus ac efficitur. Pellentesque varius lacus tortor, vel eleifend lectus fringilla quis. Pellentesque felis ex, aliquam id felis ac, dictum bibendum magna. Nunc eget consequat velit. Donec vitae dolor vitae mauris luctus mattis. Aenean placerat sed purus interdum tincidunt. Cras efficitur elementum leo in dictum. Nullam sed auctor nulla. Nunc ultricies, nibh sed condimentum dictum, libero nulla posuere ex, in ultrices leo purus ac lectus. Quisque nec orci eget diam placerat semper. Aenean nunc lorem, consequat a turpis semper, mollis porta orci. Integer non pulvinar purus.</p>
+
+            <p>Sed pretium leo ex, nec aliquet tortor volutpat sed. Sed iaculis quis ligula nec auctor. Vivamus pulvinar ex erat, eu posuere erat ornare vitae. Nullam tempor rhoncus sodales. Sed id venenatis nisl. Donec at fringilla ex. Sed lacinia commodo blandit. Aliquam erat volutpat.</p>
+
+            <p>Nullam consectetur risus rhoncus ligula consectetur fermentum. Nullam feugiat feugiat est sit amet pharetra. Nulla rutrum euismod ipsum non semper. Suspendisse in diam finibus, cursus augue in, pellentesque neque. Sed a mauris et diam elementum sagittis. Phasellus at nulla ut risus posuere consequat. Aliquam imperdiet id mauris malesuada imperdiet. Proin pretium elementum sodales. Cras sit amet tristique risus, ac varius diam. Duis feugiat, ligula a sollicitudin accumsan, mi ante dignissim sapien, in venenatis massa arcu sed justo.</p>
+
+            <p>Vivamus non nisi posuere, imperdiet dui at, suscipit sem. Mauris pulvinar mollis ipsum, sit amet cursus ligula efficitur ut. Nunc vel sodales mauris, eu mollis nunc. Cras varius aliquet est, a bibendum lacus gravida eu. Vivamus ac scelerisque arcu, quis laoreet nunc. Curabitur accumsan, tellus non vestibulum dignissim, libero dolor imperdiet nunc, nec tristique massa nulla eu velit. Maecenas ex quam, consectetur sit amet porta vitae, placerat et purus. Nam sit amet sodales elit. In hac habitasse platea dictumst. Cras convallis iaculis ex id molestie. Phasellus eu lobortis mauris. Duis quis placerat ligula. Sed euismod magna nec rutrum consectetur. Sed consequat convallis tellus, vel accumsan tellus. Quisque suscipit nunc vel egestas laoreet. Fusce vestibulum a mauris at tristique.</p>
+        </div>
+    </div>
+</div>
 
 <?php include VIEW_PATH . "inc/footer.php"; ?>
--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,4 +1,7 @@
 </main>
+
+<script src="<?php echo SITE_HOME . "public\scripts\jquery.js"; ?>"></script>
+<script src="<?php echo SITE_HOME . "public\scripts\app.js"; ?>"></script>
 </body>
 
 </html>
